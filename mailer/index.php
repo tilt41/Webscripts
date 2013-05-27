7997c2aab068e09bdde384e8cba92a936ae8a119
Redstoneterror <redstoneterror@gmail.com>
2013-05-27 01:01:04 -0500

    Create index.php

---
+++ mailer/index.php
@@ -0,0 +1,38 @@
+<html>
+ 
+<head>
+  <title>Contact Form</title>
+</head>
+ <body>
+<center>
+<font size="5">
+<b>
+Contact form
+</b>
+        <br />
+        <br />
+</font>
+<form method="POST" action="mailer.php">
+Subject:
+<input type="text" name="subject" size="20">
+        <br />
+        <br />
+Name:
+<input type="text" name="name" size="20">
+        <br />
+        <br />
+E-mail:
+<input type="text" name="email" size="20">
+        <br />
+        <br />
+Message:<br>
+<textarea rows="9" name="message" cols="30">
+</textarea>
+        <br />
+        <br />
+<input type="submit" value="Send" name="submit">
+</form>
+</center>
+</body>
+
+</html>
