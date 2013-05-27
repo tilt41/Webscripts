b00b3060b7722bdcd9c643430c214257f20283ea
Redstoneterror <redstoneterror@gmail.com>
2013-05-27 01:06:54 -0500

    Create index.php

---
+++ upload/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+  </head>
+  <center><br><br>
+   
+      <h1>File Upload</h1>
+ <div class="container">
+    <div class="well well-large">
+  <form action="upfile.php" method="post" enctype="multipart/form-data">
+   <p>
+      <label for="file">Select a file:</label> <input type="file" name="userfile" id="file"> 
+      <button>Upload File</button>
+   <p>
+          </center></body>
+</html>
