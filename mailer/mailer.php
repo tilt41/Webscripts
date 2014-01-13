558a20638bf9061bc02525aaf31e831c2b6f3689
Redstoneterror <redstoneterror@gmail.com>
2014-01-13 03:58:50 -0600

    Update mailer.php

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -1,8 +1,8 @@
-<?php
+<?php // This file will take care of sending the email 
 
 if(isset($_POST['submit'])) {
 
-$myemail = "You@yoursite.com";
+$myemail = "You@yoursite.com"; //This is wwhere your email will go
 $subject = $_POST['subject'];
 $name = $_POST['name'];
 $email = $_POST['email'];
